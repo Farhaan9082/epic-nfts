c9371f802f150868ada48beccc15f7dbb823980c
Farhaan Khan <farhaankhanfiroz@gmail.com>
2022-04-28 13:55:02 +0530

    Mint nft on testnet

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,10 +2,32 @@
 
 pragma solidity ^0.8.1;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
-        console.log("Your awesome NFT is up and running!!");
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // We need to pass the name of our NFTs token and its symbol.
+    constructor() ERC721 ("EpicNFTs", "ENFTs") {
+        console.log("This is my NFT contract. Woah!");
+    }
+
+    // A function our user will hit to get their NFT.
+    function makeAnEpicNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // Set the NFTs data.
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/13CD");
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,18 +1,29 @@
 const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
-    const nftContract = await nftContractFactory.deploy();
-    await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address);
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+
+  // Call the function.
+  let getNFT = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await getNFT.wait()
+
+  // Mint another NFT for fun.
+  getNFT = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await getNFT.wait()
+
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
